0a3b031118332f3f21419cb8716db7976c444425
Edwin-Antonio~ <edanarellanoma@ittepic.edu.mx>
2025-10-28 18:08:50 -0600

    New entity features implemented

--- src/main/java/com/ingedwin/springboot/jpa/app/springboot_jpa/entities/Person.java
+++ src/main/java/com/ingedwin/springboot/jpa/app/springboot_jpa/entities/Person.java
@@ -1,13 +1,39 @@
 package com.ingedwin.springboot.jpa.app.springboot_jpa.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+
 @Table(name = "persons")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
-    private String lastName;
+    private String lastname;
+
+    /*
+     * Cuando tenemos una composicion de palabras como en el caso de programmingLanguage, sql
+     * crea la instancia con "_" o sea "programming_language" por tal motivo no va a reconover
+     * la columna ya que se estructuran por defecto diferentes, así que usamos el Column para
+     * especificar el nombre de la columna y evitar errores
+     */
+
+    @Column(name = "programming_languaje") 
     private String programmingLanguage;
 }
